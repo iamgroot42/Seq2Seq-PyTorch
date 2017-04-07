b09b52b8f133821464dea8504be97f6b70df6d3d
Anshuman Suri <anshuman14021@iiitd.ac.in>
2017-04-07 11:58:43 +0530

    Bruteforce, yo :|

--- translate.py
+++ translate.py
@@ -23,6 +23,8 @@ args = parser.parse_args()
 config_file_path = args.config
 config = read_config(config_file_path)
 experiment_name = hyperparam_string(config)
+save_dir = config['data']['save_dir']
+load_dir = config['data']['load_dir']
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
@@ -40,6 +42,9 @@ formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 console.setFormatter(formatter)
 # add the handler to the root logger
 logging.getLogger('').addHandler(console)
+
+
+print 'Reading data ...'
 
 src, trg = read_nmt_data(
     src=config['data']['src'],
@@ -81,31 +86,109 @@ weight_mask = torch.ones(trg_vocab_size).cuda()
 weight_mask[trg['word2id']['<pad>']] = 0
 loss_criterion = nn.CrossEntropyLoss(weight=weight_mask).cuda()
 
+if config['model']['seq2seq'] == 'vanilla':
 
-model = Seq2SeqAttention(
-    src_emb_dim=config['model']['dim_word_src'],
-    trg_emb_dim=config['model']['dim_word_trg'],
-    src_vocab_size=src_vocab_size,
-    trg_vocab_size=trg_vocab_size,
-    src_hidden_dim=config['model']['dim'],
-    trg_hidden_dim=config['model']['dim'],
-    ctx_hidden_dim=config['model']['dim'],
-    attention_mode='dot',
-    batch_size=batch_size,
-    bidirectional=config['model']['bidirectional'],
-    pad_token_src=src['word2id']['<pad>'],
-    pad_token_trg=trg['word2id']['<pad>'],
-    nlayers=config['model']['n_layers_src'],
-    nlayers_trg=config['model']['n_layers_trg'],
-    dropout=0.,
-).cuda()
+    model = Seq2Seq(
+        src_emb_dim=config['model']['dim_word_src'],
+        trg_emb_dim=config['model']['dim_word_trg'],
+        src_vocab_size=src_vocab_size,
+        trg_vocab_size=trg_vocab_size,
+        src_hidden_dim=config['model']['dim'],
+        trg_hidden_dim=config['model']['dim'],
+        batch_size=batch_size,
+        bidirectional=config['model']['bidirectional'],
+        pad_token_src=src['word2id']['<pad>'],
+        pad_token_trg=trg['word2id']['<pad>'],
+        nlayers=config['model']['n_layers_src'],
+        nlayers_trg=config['model']['n_layers_trg'],
+        dropout=0.,
+    ).cuda()
 
-model.load_state_dict(torch.load(open(sys.argv[1])))
+elif config['model']['seq2seq'] == 'attention':
 
-preds = evaluate_model(
-    model, src, src_test, trg,
-    trg_test, config, verbose=False,
-    metric='bleu',raw=True
-)
+    model = Seq2SeqAttention(
+        src_emb_dim=config['model']['dim_word_src'],
+        trg_emb_dim=config['model']['dim_word_trg'],
+        src_vocab_size=src_vocab_size,
+        trg_vocab_size=trg_vocab_size,
+        src_hidden_dim=config['model']['dim'],
+        trg_hidden_dim=config['model']['dim'],
+        ctx_hidden_dim=config['model']['dim'],
+        attention_mode='dot',
+        batch_size=batch_size,
+        bidirectional=config['model']['bidirectional'],
+        pad_token_src=src['word2id']['<pad>'],
+        pad_token_trg=trg['word2id']['<pad>'],
+        nlayers=config['model']['n_layers_src'],
+        nlayers_trg=config['model']['n_layers_trg'],
+        dropout=0.,
+    ).cuda()
 
-print preds
+elif config['model']['seq2seq'] == 'fastattention':
+
+    model = Seq2SeqFastAttention(
+        src_emb_dim=config['model']['dim_word_src'],
+        trg_emb_dim=config['model']['dim_word_trg'],
+        src_vocab_size=src_vocab_size,
+        trg_vocab_size=trg_vocab_size,
+        src_hidden_dim=config['model']['dim'],
+        trg_hidden_dim=config['model']['dim'],
+        batch_size=batch_size,
+        bidirectional=config['model']['bidirectional'],
+        pad_token_src=src['word2id']['<pad>'],
+        pad_token_trg=trg['word2id']['<pad>'],
+        nlayers=config['model']['n_layers_src'],
+        nlayers_trg=config['model']['n_layers_trg'],
+        dropout=0.,
+    ).cuda()
+
+if load_dir:
+    model.load_state_dict(torch.load(
+        open(load_dir)
+    ))
+
+# __TODO__ Make this more flexible for other learning methods.
+if config['training']['optimizer'] == 'adam':
+    lr = config['training']['lrate']
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+elif config['training']['optimizer'] == 'adadelta':
+    optimizer = optim.Adadelta(model.parameters())
+elif config['training']['optimizer'] == 'sgd':
+    lr = config['training']['lrate']
+    optimizer = optim.SGD(model.parameters(), lr=lr)
+else:
+    raise NotImplementedError("Learning method not recommend for task")
+
+for j in xrange(0, len(src['data']), batch_size):
+
+    input_lines_src, _, lens_src, mask_src = get_minibatch(
+        src['data'], src['word2id'], j,
+        batch_size, max_length, add_start=True, add_end=True
+    )
+    input_lines_trg, output_lines_trg, lens_trg, mask_trg = get_minibatch(
+        trg['data'], trg['word2id'], j,
+        batch_size, max_length, add_start=True, add_end=True
+    )
+
+    decoder_logit = model(input_lines_src, input_lines_trg)
+
+    loss = loss_criterion(
+        decoder_logit.contiguous().view(-1, trg_vocab_size),
+        output_lines_trg.view(-1)
+    )
+    
+    losses.append(loss.data[0])
+    loss.backward()
+
+    word_probs = model.decode(
+        decoder_logit
+    ).data.cpu().numpy().argmax(axis=-1)
+        
+    for sentence_pred in word_probs[:5]:
+        sentence_pred = [trg['id2word'][x] for x in sentence_pred]
+
+    if '</s>' in sentence_pred:
+        index = sentence_pred.index('</s>')
+        sentence_pred = sentence_pred[:index]
+
+    print ' '.join(sentence_pred)
